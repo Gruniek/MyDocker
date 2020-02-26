2a6522594979ac108ebd9af97eff041d27b2ef62
Gruniek <mous.be@gmail.com>
2020-02-26 21:43:17 +0100

    Update multicraft.sh

--- Multicraft/multicraft.sh
+++ Multicraft/multicraft.sh
@@ -2,8 +2,26 @@
 
 case "$1" in
 'start')
-echo "Starting application"
-java -Xms512M -Xmx512M -jar $2 nogui
+
+  if [ $2 = "autoreboot" ]
+then
+
+
+while :
+do
+  echo "Press <CTRL+C> to exit."
+    java -Xms512M -Xmx512M -jar $2 nogui
+
+done
+else
+  echo "Starting application"
+  java -Xms512M -Xmx512M -jar $2 nogui
+fi
+
+
+
+
+
 ;;
 'update')
 echo "Update the application"
